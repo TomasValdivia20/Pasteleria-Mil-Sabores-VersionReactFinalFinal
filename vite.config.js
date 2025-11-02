7a1e4f5ebbe6fdba86f2d2081fd8b39288a8de65
kniete <fe.canete@duocuc.cl>
2025-11-02 18:30:02 -0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
   plugins: [react()],
-})
+  // Añade esta sección 'test':
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './setup.js', // <-- ¡Esta es la línea clave!
+  },
+});
